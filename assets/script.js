9185e39f1a915cb201888c00987dbf14a08c5ff6
ivancanga <55848546+ivancanga@users.noreply.github.com>
2020-01-14 23:27:32 -0300

    3rd commit

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,12 @@ const products_container = document.getElementById('products');
 const span_status = document.getElementById('span-status');
 const create_submit = document.getElementById('create-submit');
 
+const carrito = document.getElementById('carrito');
+const cart_container = document.querySelector('.cart-container');
+const items_container = document.getElementById('items-container');
+
+
+
 class Product {
     constructor(nombre, precio, categoria) {
         this.nombre = nombre;
@@ -11,15 +17,22 @@ class Product {
 }
 
 function displayQuanty() {
-    document.getElementById("quanty_items").innerHTML = `(${sessionStorage.length-1})`;
+    document.getElementById("quanty_items").innerHTML = `(${sessionStorage.length - 1})`;
 }
 
 window.onload = () => {
     if (localStorage.length > 1) {
         displayQuanty();
         displayProducts();
+        displayCart();
     }
 }
+
+carrito.addEventListener('click', (e) => {
+    cart_container.style.display = 'block';
+});
+
+
 
 create_submit.addEventListener('click', (e) => {
     createProduct();
@@ -38,7 +51,6 @@ function createProduct() {
         span_status.innerHTML = 'Producto cargado.';
         document.getElementById('nombre').value = '';
         document.getElementById('precio').value = '';
-        displayQuanty();
     } else {
         span_status.innerHTML = 'Datos incorrectos.'
     }
@@ -57,7 +69,7 @@ function displayProducts() {
             <p class='prod_cat'>${ObjProduct.categoria}</p>
             <p class='prod_name'>${ObjProduct.nombre}</p>
             <p class='prod_price'>$${ObjProduct.precio}</p>
-            </div>`
+            </div>`;
             btn_add = document.createElement('button');
             btn_add.className = 'add-item';
             btn_add.innerHTML = 'Agregar';
@@ -65,9 +77,45 @@ function displayProducts() {
             btn_add.addEventListener('click', (e) => {
                 sessionStorage.setItem(`item-${e.target.id}`,localStorage.getItem(localStorage.key(i)));
                 displayQuanty();
+                displayCart();
             });
             products_container.append(product_box);
             product_box.append(btn_add);
         }
     }
 };
+
+
+function displayCart() {
+    items_container.innerHTML = '';
+    if (localStorage.length > 0) {
+        for (key in sessionStorage) {
+            if (sessionStorage.getItem(key) !== 'true') {
+                const ObjItem = JSON.parse(sessionStorage.getItem(key));
+                let item_box = document.createElement('div');
+                item_box.className = 'item_box';
+                item_box.innerHTML = `
+                <img class='cart-img' src="/cruce/assets/images/img_reloj.png" alt="">
+                <p>${ObjItem.nombre}</p>
+                <p>$${ObjItem.precio}</p>`;
+                btn_remove = document.createElement('p');
+                btn_remove.innerHTML = 'Quitar';
+                btn_remove.className = 'remove-btn';
+                btn_remove.id = key;
+                btn_remove.addEventListener('click', (e) => {
+                    sessionStorage.removeItem(e.target.id);
+                    displayCart();
+                })
+                items_container.append(item_box);
+                item_box.append(btn_remove);
+            }
+        }
+    }
+}
+
+const info_cart = document.createElement('div');
+info_cart.innerHTML = `
+    <button class='btn-cart'>Finalizar compra</button>
+    <a href='#'>Seguir comprando</a>
+`;
+cart_container.append(info_cart);
